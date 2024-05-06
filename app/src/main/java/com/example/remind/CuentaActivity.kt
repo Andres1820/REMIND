17989965bfa2fa66720410c93e600d4e68a2b71b
Andres1820 <andresdavid1820@gmail.com>
2024-05-05 21:51:12 -0500

    Se agregan dos nuevas actividades

--- app/src/main/java/com/example/remind/CuentaActivity.kt
+++ app/src/main/java/com/example/remind/CuentaActivity.kt
@@ -3,37 +3,36 @@ package com.example.remind
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class CuentaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_cuenta)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-        ingresar()
-        registrarse()
+        cerrarSesion()
+        inicio()
     }
-    private fun ingresar(){
-        val button :Button = findViewById(R.id.btnIngresar)
+    private fun inicio (){
+        val button : Button = findViewById(R.id.btnVolver)
         button.setOnClickListener{
             val intent = Intent(this, InicioActivity::class.java)
             startActivity(intent)
         }
     }
-    private fun registrarse(){
-        val button :Button = findViewById(R.id.btnRegistrarse)
+    private fun cerrarSesion (){
+        val button : Button = findViewById(R.id.btnCerrarSesion)
         button.setOnClickListener{
-            Toast.makeText(this, "Dialogo de registro", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
--- app/src/main/java/com/example/remind/InicioActivity.kt
+++ app/src/main/java/com/example/remind/InicioActivity.kt
@@ -3,37 +3,28 @@ package com.example.remind
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class InicioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_inicio)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
-        ingresar()
-        registrarse()
+        cuenta()
     }
-    private fun ingresar(){
-        val button :Button = findViewById(R.id.btnIngresar)
+    private fun cuenta(){
+        val button : Button = findViewById(R.id.btnCuenta)
         button.setOnClickListener{
-            val intent = Intent(this, InicioActivity::class.java)
+            val intent = Intent(this, CuentaActivity::class.java)
             startActivity(intent)
-        }
-    }
-    private fun registrarse(){
-        val button :Button = findViewById(R.id.btnRegistrarse)
-        button.setOnClickListener{
-            Toast.makeText(this, "Dialogo de registro", Toast.LENGTH_SHORT).show()
         }
     }
 }
